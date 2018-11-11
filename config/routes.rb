0886a223647643327e90e405ac04c94bfb4b2c61
s.sato <samurai310@aol.jp>
2018-11-11 06:08:38 +0000

    トップページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'top#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :favorites, only: [:create, :destroy]
