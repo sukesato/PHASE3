1c31af53e22c2c856265c9b2e9b2f74bb84fde5a
s.sato <samurai310@aol.jp>
2018-11-14 10:56:59 +0000

    メンター指摘事項の修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,4 +8,8 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to insts_url, notice: "#{favorite.inst.user.name}さんの投稿をお気に入り解除しました"
   end
+  
+  def index
+    @favorites = current_user.favorites
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   
   resources :insts do
     collection do
