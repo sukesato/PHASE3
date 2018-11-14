be9be07d3287b3a4a621ead295f07bcf0e8a50fb
s.sato <samurai310@aol.jp>
2018-11-14 08:04:52 +0000

    画像投稿時に確認メールが届かない件対応

--- app/mailers/inst_mailer.rb
+++ app/mailers/inst_mailer.rb
@@ -2,6 +2,6 @@ class InstMailer < ApplicationMailer
   def inst_mail(inst)
     @inst = inst
 
-    mail to: "samurai310@aol.jp", subject: "お問い合わせの確認メール"
+    mail to: @inst.user.email, subject: "お問い合わせの確認メール"
   end
 end
